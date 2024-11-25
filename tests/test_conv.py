d50968792910875545eee929172f1c386cbab594
Sean20405 <sean20405.cs11@nycu.edu.tw>
2024-11-25 10:53:03 +0800

    add: test correctness of fftconvolve2d

--- tests/test_conv.py
+++ tests/test_conv.py
@@ -1,3 +1,4 @@
+import scipy
 import fft
 import numpy as np
 import random
@@ -13,3 +14,15 @@ def test_conv():
 
     assert np.array(x_conv).shape == (x_size, x_size)
     assert np.array(x_conv).dtype == np.float64
+
+def test_correctness():
+    x_size = 512
+    kernel_size = 15
+
+    x = np.array([[random.randint(0, 255) for _ in range(x_size)] for _ in range(x_size)])  # image-like
+    kernel = np.array([[random.uniform(0, 1) for _ in range(kernel_size)] for _ in range(kernel_size)])
+
+    x_fft = fft.fftconvolve2d(x, kernel)
+    x_scipy = scipy.signal.convolve2d(x, kernel, mode='same')
+
+    assert np.array(x_fft) == pytest.approx(x_scipy)
