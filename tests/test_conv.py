2b714dcaf4f8a1737772fcbb0ae0237caf590926
Sean20405 <sean20405.cs11@nycu.edu.tw>
2024-12-05 15:34:38 +0800

    add: test for all mode

--- tests/test_conv.py
+++ tests/test_conv.py
@@ -4,17 +4,6 @@ import numpy as np
 import random
 import pytest
 
-def test_conv():
-    x_size = 256
-    kernel_size = 5
-
-    x = np.array([[random.randint(0, 255) for _ in range(x_size)] for _ in range(x_size)])  # image-like
-    kernel = np.array([[random.uniform(0, 1) for _ in range(kernel_size)] for _ in range(kernel_size)])
-    x_conv = fft.fftconvolve2d(x, kernel)
-
-    assert np.array(x_conv).shape == (x_size, x_size)
-    assert np.array(x_conv).dtype == np.float64
-
 def test_correctness():
     x_size = 512
     kernel_size = 15
@@ -22,7 +11,21 @@ def test_correctness():
     x = np.array([[random.randint(0, 255) for _ in range(x_size)] for _ in range(x_size)])  # image-like
     kernel = np.array([[random.uniform(0, 1) for _ in range(kernel_size)] for _ in range(kernel_size)])
 
-    x_fft = fft.fftconvolve2d(x, kernel)
+    x_fft = fft.fftconvolve2d(x, kernel, mode='same')
     x_scipy = scipy.signal.convolve2d(x, kernel, mode='same')
 
     assert np.array(x_fft) == pytest.approx(x_scipy)
+
+def test_mode():
+    x_size = 512
+    kernel_size = 15
+
+    x = np.array([[random.randint(0, 255) for _ in range(x_size)] for _ in range(x_size)])  # image-like
+    kernel = np.array([[random.uniform(0, 1) for _ in range(kernel_size)] for _ in range(kernel_size)])
+
+    modes = ['full', 'valid', 'same']
+    for mode in modes:
+        x_fft = fft.fftconvolve2d(x, kernel, mode=mode)
+        x_scipy = scipy.signal.convolve2d(x, kernel, mode=mode)
+
+        assert np.array(x_fft) == pytest.approx(x_scipy)
