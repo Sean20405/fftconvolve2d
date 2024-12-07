aa7638fc77ef34771572b0d7b84489a9e548b159
Sean20405 <sean20405.cs11@nycu.edu.tw>
2024-12-07 16:28:18 +0800

    fix: test script for Gaussian blur

--- test_gaussian.py
+++ test_gaussian.py
@@ -5,20 +5,31 @@ import cv2
 
 img = cv2.imread('assets/Lenna.png').astype(np.float64)
 orig_h, orig_w, _ = img.shape
-# img = cv2.resize(img, (4062, 4062))
+img = cv2.resize(img, (4096, 4096))
 h, w, _ = img.shape
 
 gaussianKernel1d = cv2.getGaussianKernel(35, 10.0)
 gaussianKernel2d = np.outer(gaussianKernel1d, gaussianKernel1d)
 
-output_img = np.zeros((h, w, 3), dtype=np.float64)
-output_img_scipy = np.zeros((h, w, 3), dtype=np.float64)
-output_img_scipy_fft = np.zeros((h, w, 3), dtype=np.float64)
+mode = 'full'
+
+if mode == 'full':
+    output_shape = (h + 34, w + 34, 3)
+elif mode == 'same':
+    output_shape = (h, w, 3)
+elif mode == 'valid':
+    output_shape = (h - 34, w - 34, 3)
+
+output_img = np.zeros(output_shape, dtype=np.float64)
+output_img_scipy = np.zeros(output_shape, dtype=np.float64)
+output_img_scipy_fft = np.zeros(output_shape, dtype=np.float64)
+
+mode = 'full'
 
 for channel in range(3):
-    output_channel = fft.fftconvolve2d(img[:, :, channel], gaussianKernel2d)
-    output_scipy = scipy.signal.convolve2d(img[:, :, channel], gaussianKernel2d, mode='same')
-    output_scipy_fft = scipy.signal.fftconvolve(img[:, :, channel], gaussianKernel2d, mode='same')
+    output_channel = fft.fftconvolve2d(img[:, :, channel], gaussianKernel2d, mode=mode, method='cooley_tukey')
+    output_scipy = scipy.signal.convolve2d(img[:, :, channel], gaussianKernel2d, mode=mode)
+    output_scipy_fft = scipy.signal.fftconvolve(img[:, :, channel], gaussianKernel2d, mode=mode)
 
     output_img[:, :, channel] = output_channel
     output_img_scipy[:, :, channel] = output_scipy
