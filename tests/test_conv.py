bc5412b491af700e244f831fce8b7c8c9e742904
Sean20405 <sean20405.cs11@nycu.edu.tw>
2024-12-13 16:36:50 +0800

    add: more comprehensive test

--- tests/test_conv.py
+++ tests/test_conv.py
@@ -46,4 +46,16 @@ def test_array():
     assert np.array(x_fft) == pytest.approx(x_scipy)
     assert x_scipy == pytest.approx(x_np_fft)
 
-# test_array()
+def test_method():
+    x_size = 512
+    kernel_size = 15
+
+    x = np.array([[random.randint(0, 255) for _ in range(x_size)] for _ in range(x_size)])  # image-like
+    kernel = np.array([[random.uniform(0, 1) for _ in range(kernel_size)] for _ in range(kernel_size)])
+
+    x_scipy = scipy.signal.convolve2d(x, kernel, mode='same')
+
+    methods = ['cooley_tukey', 'mixed_radix']
+    for method in methods:
+        x_fft = fft.fftconvolve2d(x, kernel, mode='same', method=method)
+        assert np.array(x_fft) == pytest.approx(x_scipy)
--- tests/test_fft.py
+++ tests/test_fft.py
@@ -3,18 +3,35 @@ import numpy as np
 import random
 import pytest
 
-def test_1d():
-    length = 16
-    x = np.array([random.uniform(-length, length) for _ in range(length)])
-    x_fft = fft.fft1d(x)
-    x_ifft = fft.ifft1d(x_fft)
-
-    assert np.array(x_ifft) == pytest.approx(x)
-
-def test_2d():
+def test_correctness():
     height, width = 16, 16
     x = np.array([[random.uniform(-width, width) for _ in range(width)] for _ in range(height)])
-    x_fft = fft.fft2d(x)
-    x_ifft = fft.ifft2d(x_fft)
 
-    assert np.array(x_ifft) == pytest.approx(x)
+    x_np_fft = np.fft.fft2(x)
+    methods = ['cooley_tukey', 'mixed_radix']
+    for method in methods:
+        x_fft = fft.fft2d(x, method=method)
+        assert np.array(x_fft) == pytest.approx(x_np_fft)
+
+def test_size():
+    heights = [16, 64, 256]
+    widths = [16, 64, 256]
+
+    for height, width in zip(heights, widths):
+        x = np.array([[random.uniform(-width, width) for _ in range(width)] for _ in range(height)])
+
+        x_np_fft = np.fft.fft2(x)
+        methods = ['cooley_tukey', 'mixed_radix']
+        for method in methods:
+            x_fft = fft.fft2d(x, method=method)
+            assert np.array(x_fft) == pytest.approx(x_np_fft)
+
+def test_inverse():
+    height, width = 16, 16
+    x = np.array([[random.uniform(-width, width) for _ in range(width)] for _ in range(height)])
+
+    methods = ['cooley_tukey', 'mixed_radix']
+    for method in methods:
+        x_fft = fft.fft2d(x, method=method)
+        x_ifft = fft.ifft2d(x_fft, method=method)
+        assert np.array(x_ifft) == pytest.approx(x)
